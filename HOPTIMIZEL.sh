96fa618b2d6a618d900e42cc3b62e143b493a685
haidarbahzi <biglamp.gang@gmail.com>
2022-10-15 13:32:28 +0700

    Menu Test

--- HOPTIMIZEL.sh
+++ HOPTIMIZEL.sh
@@ -3,20 +3,20 @@
 #Author : Haidar Bahzi
 #Github : haidarbahzi
 
-echo.
-echo.
-echo.
-echo        ==========================================================================================================
-echo         II      II   IIIIIIII   IIIIIIIIII  IIIIIIIIII  II  IIIIIIIIIIII  II  IIIIIIIIII  IIIIIIIIII  II
-echo         II      II  II      II  II      II      II      II  II   II   II  II        II    II          II
-echo         II      II  II      II  II      II      II      II  II   II   II  II       II     II          II
-echo         IIIIIIIIII  II      II  IIIIIIIIII      II      II  II   II   II  II      II      IIIIIIIIII  II
-echo         II      II  II      II  II              II      II  II   II   II  II     II       II          II
-echo         II      II  II      II  II              II      II  II   II   II  II    II        II          II
-echo         II      II   IIIIIIII   II              II      II  II   II   II  II  IIIIIIIIII  IIIIIIIIII  IIIIIIIIII
-echo.
-echo                                                     Linux Edition
-echo        ==========================================================================================================
-echo.
-echo.
-echo.
+echo
+echo
+echo
+echo ==========================================================================================================
+echo II      II   IIIIIIII   IIIIIIIIII  IIIIIIIIII  II  IIIIIIIIIIII  II  IIIIIIIIII  IIIIIIIIII  II
+echo II      II  II      II  II      II      II      II  II   II   II  II        II    II          II
+echo II      II  II      II  II      II      II      II  II   II   II  II       II     II          II
+echo IIIIIIIIII  II      II  IIIIIIIIII      II      II  II   II   II  II      II      IIIIIIIIII  II
+echo II      II  II      II  II              II      II  II   II   II  II     II       II          II
+echo II      II  II      II  II              II      II  II   II   II  II    II        II          II
+echo II      II   IIIIIIII   II              II      II  II   II   II  II  IIIIIIIIII  IIIIIIIIII  IIIIIIIIII
+echo
+echo                                             Linux Edition
+echo ==========================================================================================================
+echo
+echo
+echo
